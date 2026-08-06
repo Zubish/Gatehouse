67becef79539009538e53c82eaa80517bc4d3c0c
Zubish <lordbish98@gmail.com>
2026-08-06 20:50:51 +0100

    fix(render): Wrap App component in GatehouseProvider in main.tsx to resolve black screen crash

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GatehouseProvider } from './context/GatehouseContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GatehouseProvider>
+      <App />
+    </GatehouseProvider>
   </StrictMode>,
 )
